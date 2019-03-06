323895e2e0aeb9aa71c37030d66ce8a3d70a466f
Mika Vainio <koti.mika.vainio@gmail.com>
2019-03-06 20:18:30 +0200

    Added console operations

--- JasenSovellus/Program.cs
+++ JasenSovellus/Program.cs
@@ -42,6 +42,16 @@ namespace JasenSovellus
         {
             return this.name;
         }
+
+        // Method for displaying all properties of an object
+        public void showProperties()
+        {
+            Console.WriteLine("Yhdistyksen nimi on " + this.name);
+            Console.WriteLine("Yhdistyksen katuosoite on " + this.street);
+            Console.WriteLine("Yhdistyksen postinumero on " + this.zipCode);
+            Console.WriteLine("Yhdistyksen postitoimipaikka " + this.city);
+            Console.WriteLine("Ja jäsenmäärä on " + this.members.ToString());
+        }
     }
 
     // Main program starts here
@@ -49,10 +59,31 @@ namespace JasenSovellus
     {
         static void Main(string[] args)
         {
+            // Lets type association information
+            string inputName;
+            string inputStreet;
+            string inputZipCode;
+            string inputCity;
+            string inputMembers;
+            uint members;
+
+            Console.Write("Syötä yhdistyksen nimi: ");
+            inputName = Console.ReadLine();
+            Console.Write("Anna katuosoite: ");
+            inputStreet = Console.ReadLine();
+            Console.Write("Anna postinumero: ");
+            inputZipCode = Console.ReadLine();
+            Console.Write("Syötä postitoimipaikka: ");
+            inputCity = Console.ReadLine();
+            Console.Write("Syötä jäsenmäärä: ");
+            inputMembers = Console.ReadLine();
+            members = Convert.ToUInt32(inputMembers);
+
+
             // Lets create a Association object named association
-            Association association = new Association();
+            Association association = new Association(inputName, inputStreet, inputZipCode, inputCity, members);
             string nimi = association.showName();
-            Console.WriteLine(nimi);
+            association.showProperties();
             Console.ReadLine();
             
         }
