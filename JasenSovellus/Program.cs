08c4328b88c81fe49180a264ada028710953b947
Mika Vainio <koti.mika.vainio@gmail.com>
2019-03-06 16:27:12 +0200

    Päiviys

--- JasenSovellus/Program.cs
+++ JasenSovellus/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace JasenSovellus
 {
+    // Classes for assiciations and members
+
+    // Class for the Accociation
+    class Accociation
+    {
+        // Defined fields
+        string name;
+        string street;
+        string zipCode;
+        string city;
+        uint members;
+
+        // Default constructor, no parameters
+        public Accociation()
+        {
+            this.name = "N/A";
+            this.street = "N/A";
+            this.zipCode = "N/A";
+            this.city = "N/A";
+            this.members = 0;
+        }
+
+        public string showName()
+        {
+            return this.name;
+        }
+    }
+
+    // Main program starts here
     class Program
     {
         static void Main(string[] args)
         {
+            // Lets create a Accociation object named accociation
+            Accociation accociation = new Accociation();
+            string nimi = accociation.showName();
+            Console.WriteLine(nimi);
+            Console.ReadLine();
+            
         }
     }
 }
